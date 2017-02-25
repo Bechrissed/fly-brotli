85a69361400a14455d572b79b2550aed3558de04
Chris <koster.cl@gmail.com>
2017-02-25 15:04:39 +0100

    string

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ const compress = require('brotli/compress');
 const defs = require('./config');
 
 module.exports = {
-        name: 'brotli',
-    * func(file, opts){
-    opts = Object.assign({}, defs, opts);
-    const comp = compress(fs.readFileSync(file), opts);
-    fs.writeFile('file.br', comp, function (err) {
-        if (err) return console.log(err);
-        console.log('Hello World > helloworld.txt');
-    });
+	name: 'brotli',
+	* func(file, opts) {
+		opts = Object.assign({}, defs, opts);
+
+        const comp = compress(fs.readFileSync(file.data.toString()), opts);
+        fs.writeFile('file.br', comp, function (err) {
+            if (err) return console.log(err);
+            console.log('Hello World > helloworld.txt');
+        });
+	}
 };
